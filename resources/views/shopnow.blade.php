35dfe5e234c8b6d57eb38c04f63e76402b2a046e
pragathi318 <garipallypragathi@gmail.com>
2026-02-07 22:25:25 +0530

    Standardize activities routes, refine footer/header aesthetics, and optimize mobile view

--- resources/views/shopnow.blade.php
+++ resources/views/shopnow.blade.php
@@ -390,7 +390,7 @@
                 </div>
             </div>
 
-            <a href="/Activities" class="btn-explore">
+            <a href="/activities" class="btn-explore">
                 Explore Activities
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
                     stroke-width="2">
